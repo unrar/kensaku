f8e309df5f5ab71f5715996cd15f8df6d7748cf3
unrar <catbuntu@catbuntu.me>
2014-04-10 21:41:04 +0200

    Updating NodeServer to 0.2a - Adding the Hello solution

--- solutions/nodeserver/server.js
+++ solutions/nodeserver/server.js
@@ -1,20 +1,28 @@
 // BEGIN CONFIG
-c2url = "http://cloudium-kensaku.khalin.com"; //URL to your C2 machine (remember to NOT include the *arguments* [username, type])
-c2user = "afm";
-c2pwd = "natoar23ae";
-c2plan = "paas";
+c2url = "http://myapp.kensakuapp.com"; //URL to your C2 machine (remember NOT to include the *arguments* [username, type])
+c2user = "myusername";
+c2pwd = "mypassword";
+c2plan = "iaas";
 c2full = c2url + " " + c2user + " " + c2plan;
 // Welcome message
-console.log("Welcome to NodeServer v0.1a-c2.");
+console.log("Welcome to NodeServer v0.2a-c2.");
 
 // Khalin C2 block
-console.log("This NodeServer is compiled to use the Khalin Cloudium2 server with the following data:");
+console.log("NodeServer has been bundled to work with KensakuApp and it will use the following information:");
 console.log("URL: 		" + c2url);
 console.log("User: 		" + c2user);
 console.log("Password: 	" + c2pwd);
 console.log("Plan: 		" + c2plan);
 console.log("Full URL 	" + c2full)
 console.log("Setting server up...");
+
+// Get the port
+//DEFAULT PORT = 8080
+var port = 8080;
+if (process.argv.length >= 3) {
+  var myArgs = process.argv.slice(2);
+  port = myArgs[0];
+}
 var http = require('http');
 
 var server = http.createServer(function(req,res) {
@@ -28,15 +36,15 @@ var server = http.createServer(function(req,res) {
 	res.write("want to easily have an unique server for their bussiness. It allows you to easily");
 	res.write("write a server with Node.js; using custom functions like \"startHTML\", so you won't");
 	res.write("have to rewrite code never!</p>\n");
-	res.write("<div style='font-size:small;text-align:right;'>@NodeServer v0.1a <br />\n");
+	res.write("<div style='font-size:small;text-align:right;'>@NodeServer v0.2a <br />\n");
 	
 	res.write("Hosted by Kensaku.</div>\n");
 	endHTML(res);
 });
-server.listen(8080);
+server.listen(port);
 console.log("NodeServer   Start				[OK  ]");
-console.log("NodeServer  Listen 				[8080]");
-console.log("NodeServer   Visit       " + c2url + ":8080");
+console.log("NodeServer  Listen 				[" + port + "]");
+console.log("NodeServer   Visit       " + c2url + ":" + port);
 
 // These functions write the main HTML structure.
 
